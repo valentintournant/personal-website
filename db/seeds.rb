e5b8657cdb3a9e886c8a66591cdcd42e1d243508
Valentin Tournant <valentin.tournant@gmail.com>
2020-09-14 16:35:48 +0200

    some bug fix and creeation of the card

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# exercises
+puts "destroy exercises"
+Exercise.destroy_all
+
+# exercises-JS
+puts "create exercise"
+
+exercise_1_js = Exercise.create!(
+  name: "Drum Kit",
+  description: "Creation of a virtual drum kit that produces a sound when you press the associated key!",
+  category: "Javascript",
+  time: 20,
+  number: 1,
+)
+
+puts "You have done #{Exercise.count} exercise"
