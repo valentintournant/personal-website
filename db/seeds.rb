9a847aec82fb9eb31f9b4f7c2373b0a51b68f021
Valentin Tournant <valentin.tournant@gmail.com>
2020-09-15 15:26:11 +0200

    finalisation card done

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,4 +13,20 @@ exercise_1_js = Exercise.create!(
   number: 1,
 )
 
+exercise_2_js = Exercise.create!(
+  name: "test",
+  description: "Creation of a virtual drum kit that produces a sound when you press the associated key!",
+  category: "Javascript",
+  time: 20,
+  number: 1,
+)
+
+exercise_3_js = Exercise.create!(
+  name: "test",
+  description: "Creation of a virtual drum kit that produces a sound when you press the associated key!",
+  category: "Javascript",
+  time: 20,
+  number: 1,
+)
+
 puts "You have done #{Exercise.count} exercise"
