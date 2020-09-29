4103d775c277cb4b6cf9bc8fb49fa49f9a206b66
Valentin Tournant <valentin.tournant@gmail.com>
2020-09-29 18:35:54 +0200

    changing the JS to swtich the logo and the bacjground color

---
+++ app/javascript/your_plugins/colorSlider.js
@@ -0,0 +1,6 @@
+const colorSlider = () => {
+  const color = document.querySelector(".sec");
+  console.log(color);
+}
+
+export {colorSlider};
--- app/javascript/your_plugins/imgSlider.js
+++ app/javascript/your_plugins/imgSlider.js
@@ -3,9 +3,11 @@ const imgSlider = () => {
   if (img){
     const logos = document.querySelectorAll(".logo");
     logos.forEach((logo) => {
-      logo.addEventListener('click', () => {
-        console.log(logo.innerHTML);
-        img.innerHTML = logo.innerHTML;
+      logo.addEventListener('click', (event) => { // ne pas oublier l'event
+        img.innerHTML = event.currentTarget.innerHTML;
+        console.log(event.currentTarget.dataset);
+        const backgroundColor = event.currentTarget.data.background;
+        document.body.style.backgroundColor = backgroundColor;
       })
     });
   }
