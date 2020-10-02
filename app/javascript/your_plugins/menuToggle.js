3db84ab163b35e877124d2364c8ce9f52ed5d498
Valentin Tournant <valentin.tournant@gmail.com>
2020-10-02 16:25:40 +0200

    add resposive home page / js fonctionalie

---
+++ app/javascript/your_plugins/menuToggle.js
@@ -0,0 +1,12 @@
+const menuToggle = () => {
+  const menu = document.querySelector(".toggleMenu");
+  if (menu){
+    const navigation = document.querySelector(".navigation");
+    menu.addEventListener('click', (event) => {
+      menu.classList.toggle('active')
+      navigation.classList.toggle('active')
+    });
+  }
+}
+
+export {menuToggle};
