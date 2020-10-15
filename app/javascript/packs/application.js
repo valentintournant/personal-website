c1f3f1610a532034939edadf116544661c67a0f3
Valentin Tournant <valentin.tournant@gmail.com>
2020-10-15 14:58:51 +0200

    add slick-carrousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -28,7 +26,6 @@ import {imgSlider} from "../your_plugins/imgSlider";
 import {menuToggle} from "../your_plugins/menuToggle";
 import {animation} from "../your_plugins/animation";
 import {loadDynamicText} from "../your_plugins/typed";
-
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
---
+++ app/javascript/your_plugins/slick.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.logo-slider').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+  });
+});
