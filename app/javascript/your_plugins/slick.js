17fc9b73e5d0e7bcc4f46d7501765cb7a657dafc
Valentin Tournant <valentin.tournant@gmail.com>
2020-10-19 16:54:32 +0200

    creation of the responsive for the slider

--- app/javascript/your_plugins/slick.js
+++ app/javascript/your_plugins/slick.js
@@ -14,8 +14,8 @@ const slick = () => {
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
+              slidesToShow: 2,
+              slidesToScroll: 2,
               infinite: true,
               dots: true
             }
@@ -23,8 +23,8 @@ const slick = () => {
           {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2
+              slidesToShow: 1,
+              slidesToScroll: 1
             }
           },
           {
