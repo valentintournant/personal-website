b852e96a2f4da496ba723a23883ac1a6019c08bd
Valentin Tournant <valentin.tournant@gmail.com>
2020-10-19 16:39:07 +0200

    creation of scss for the compagnies section

--- app/javascript/your_plugins/slick.js
+++ app/javascript/your_plugins/slick.js
@@ -9,6 +9,32 @@ const slick = () => {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
+        arrows: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
       });
     });
   }
