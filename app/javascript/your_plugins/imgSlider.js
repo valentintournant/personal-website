63e2bf405141bd778bd28555b555d981fa4e65b4
Valentin Tournant <valentin.tournant@gmail.com>
2020-10-21 14:24:25 +0200

    test

--- app/javascript/your_plugins/imgSlider.js
+++ app/javascript/your_plugins/imgSlider.js
@@ -1,8 +1,8 @@
 const imgSlider = () => {
   const img = document.querySelector(".imgBox");
   if (img){
-    const logos = document.querySelectorAll(".logo");
     const sec = document.querySelector(".sec");
+    const logos = document.querySelectorAll(".logo");
     logos.forEach((logo) => {
       logo.addEventListener('click', (event) => {
         img.innerHTML = event.currentTarget.innerHTML;
